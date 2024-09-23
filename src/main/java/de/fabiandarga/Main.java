31289124b472f4546369aa0866ae549417c19d8c
Fabian Darga <kontakt@fabiandarga.de>
2024-09-23 14:48:44 +0200

    Add items in loop

--- src/main/java/de/fabiandarga/Main.java
+++ src/main/java/de/fabiandarga/Main.java
@@ -3,6 +3,7 @@ package de.fabiandarga;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,22 +11,28 @@ public class Main {
         String user = "numbers_and_colors_admin";
         String password = "123456";
 
+        String[] colors = {"yellow", "red", "blue", "green"};
+        Random random = new Random();
+
         Connection conn = null;
 
-        // serialize it
         try {
             conn = DriverManager.getConnection(url, user, password);
 
             String sql = "INSERT INTO pairs (number, color) VALUES (?, ?)";
+            int i = 0;
 
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setInt(1, 1000);
-            statement.setString(2, "red");
+            for (; i < 10000; i++) {
+                PreparedStatement statement = conn.prepareStatement(sql);
 
-            statement.executeUpdate();
+                statement.setInt(1, random.nextInt(1, 1001));
 
+                int colorIndex = random.nextInt(colors.length);
+                statement.setString(2, colors[colorIndex]);
+                statement.executeUpdate();
+            }
 
-            System.out.println("Objects created");
+            System.out.println(i + " Objects created");
 
         } catch (Exception ex) {
             ex.printStackTrace();
